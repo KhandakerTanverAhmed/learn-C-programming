1a8382a089e8f1144fbd27f983c4053c9513f023
Khandaker Tanver Ahmed <164225789+Tanver283@users.noreply.github.com>
2024-03-21 15:46:46 +0600

    Add files via upload

---
+++ Conditionals/OddEven/OddEven.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int x;
+    printf("The positive integer is ");
+    scanf("%d", &x);
+
+    if( x % 2 == 0 ) printf("Even Number");
+    if( x % 2!= 0 ) printf("Odd Number");
+    
+return 0;
+}
