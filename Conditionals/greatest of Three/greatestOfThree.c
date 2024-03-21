0222fa51f49ad39896af0e5a84bdfacddaea1b8d
Khandaker Tanver Ahmed <164225789+Tanver283@users.noreply.github.com>
2024-03-21 21:47:26 +0600

    Add files via upload

---
+++ Conditionals/greatest of Three/greatestOfThree.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main (){
+    int x;
+    printf(" Enter a positive integer x ");
+    scanf("%d", &x);
+
+    int y;
+    printf(" Enter a positive integer y ");
+    scanf("%d", &y);
+
+    int z;
+    printf(" Enter a positive integer z ");
+    scanf("%d", &z);
+
+    if(x>=y && x>=z) printf (" x is greater than all ");
+
+    else if(y>=x && y>=z) printf (" y is greater than all ");
+
+    else if(z>=x && z>=y) printf (" z is greater than all ");
+
+    return 0;
+}
