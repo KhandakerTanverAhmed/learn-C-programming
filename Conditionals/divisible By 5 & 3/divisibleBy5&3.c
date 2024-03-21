a628dc5d9bcd29ecf709ba725caee87846895877
Khandaker Tanver Ahmed <164225789+Tanver283@users.noreply.github.com>
2024-03-21 16:08:22 +0600

    Add files via upload

---
+++ Conditionals/divisible By 5 & 3/divisibleBy5&3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+    int x;
+    printf(" Enter a positive integer ");
+    scanf("%d", &x);
+
+     if( x%5==0 && x%3==0) printf("divisible by 5 or 3");
+     else printf("not divisible by 5 or 3");
+    return 0;
+}
