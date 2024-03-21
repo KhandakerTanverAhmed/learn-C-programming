a27bf037b514e705eca351476acee1169339346f
Khandaker Tanver Ahmed <164225789+Tanver283@users.noreply.github.com>
2024-03-21 15:12:40 +0600

    Add files via upload

---
+++ FloatDataType/floatDataType.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+    float x = 45; 
+    float y = 10;
+    printf("%f %f\n",x,y);
+    printf("%f\n", x+y);
+    printf("%f\n", x-y);
+    printf("%f\n", x*y);
+    printf("%f\n", x/y);
+
+
+return 0;
+}
