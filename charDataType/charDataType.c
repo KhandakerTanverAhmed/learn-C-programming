e450501a0a6a9888340262686d38e04348577aea
Khandaker Tanver Ahmed <164225789+Tanver283@users.noreply.github.com>
2024-03-21 15:32:56 +0600

    Add files via upload

---
+++ charDataType/charDataType.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main (){
+    char ch = 'a';
+    printf("%c\n", ch);
+
+    char cha = 'b';
+    printf("%d\n", cha);
+
+    char has = '#';
+    printf("%d\n", has);
+
+    char at = '@';
+    int ascii = (int)at;
+    printf("%d\n", ascii);
+
+    int g = 34;
+    printf("%c\n", g);
+
+    int f = 37;
+    char chab = (char)f;
+    printf("%c", chab);
+
+return 0;
+}
