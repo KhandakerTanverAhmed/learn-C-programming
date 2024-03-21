ea5017e9ff8cde3c4be9c8046b66282312ebec9c
Khandaker Tanver Ahmed <164225789+Tanver283@users.noreply.github.com>
2024-03-21 15:31:37 +0600

    Add files via upload

---
+++ halfOfNumber/halfOfNumber.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (){
+    int x;
+    printf("the integer number is ");
+    scanf("%d", &x);
+    float f = (float)x;
+    printf("half of the integer is %f", f/2);
+
+return 0;
+}
