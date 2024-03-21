69bc20a5384a5cfa878f680f7b1886b68e54df1a
Khandaker Tanver Ahmed <164225789+Tanver283@users.noreply.github.com>
2024-03-21 13:57:13 +0600

    Add files via upload

---
+++ Structure of C/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (){
+printf("my name is tanver\n");
+printf("i am doing CSE from UIU\n");
+printf("I live in Purbo vatara\n");
+printf("My Id 0112410283\n");
+
+
+return 0;
+}
