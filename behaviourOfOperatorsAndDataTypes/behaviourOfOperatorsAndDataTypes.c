11153642331189b02966070e49c1c6b7884b5921
Khandaker Tanver Ahmed <164225789+Tanver283@users.noreply.github.com>
2024-03-21 15:36:12 +0600

    Add files via upload

---
+++ behaviourOfOperatorsAndDataTypes/behaviourOfOperatorsAndDataTypes.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main (){
+    float f = 5/2;
+    printf("%f\n", f);
+
+    float a = 5;
+    float b = 2;
+    float e = a/b;
+    printf("%f\n", e);
+
+    float t = 5.0/2;
+    printf("%f\n", t);
+
+    float k = 5/2.0;
+    printf("%f\n", k);
+
+
+
+    float d = 2/7;
+    printf("%f\n", d);
+
+return 0;
+}
